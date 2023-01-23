f8a5e37cf5a3cf0485d209eaef35208d3213e43e
Antonio <59914703+AMN001@users.noreply.github.com>
2023-01-23 23:47:23 +0100

    Update de.php
    Enjoy for it.

--- languages/de.php
+++ languages/de.php
@@ -1,15 +1,15 @@
 <?php
 
 $lang_ext = [
-	'New Group of Tabs' => 'Neue Gruppe der Tabs',
+	'New Group of Tabs' => 'Neue Gruppe erstellen',
 	'Edit items' => 'Inhalt bearbeiten',
 	'Edit name' => 'Namen bearbeiten',
 	'Delete group' => 'Gruppe löschen',
 	'New Item' => 'Neuer Inhalt',
 	'Name of Group' => 'Name der Gruppe',
 	'Display page in tab' => 'Wählen Sie die Seite um die Registerkarte anzuzeigen',
-	'Info' => 'oder lassendas Feld leer um im Feld "content" anzuzeigen',
-	'ts' => 'Speichern Sie die Registerkarten Zustand auf Seite neu laden?',
+	'Info' => 'oder lassen Sie das Feld leer um im Feld "content" anzuzeigen',
+	'ts' => 'Zustand der Registerkarten beim Neuladen der Seite speichern?',
 ];
 
 
